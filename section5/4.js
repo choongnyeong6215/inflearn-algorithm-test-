f5d4c5d50e105352a0e14b4b8c0de8e70917a7e7
choongnyeong6215 <choong6215@gmail.com>
2024-02-13 22:12:10 +0900

    section5 3, 4 연속 부분수열 풀이

---
+++ section5/4.js
@@ -0,0 +1,23 @@
+function solution(m, arr) {
+    let cnt = 0;
+    let sum = 0;
+
+    // 각 요소 참조할 인덱스
+    let idx1 = idx2 = 0;
+
+    for (idx2; idx2 < arr.length; idx2++) {
+        sum += arr[idx2];
+
+        while (sum > m) {
+            sum -= arr[idx1++];
+        }
+
+        cnt += (idx2 - idx1 + 1);
+    }
+
+    return cnt;
+}
+
+
+// test case
+console.log(solution(5, [1, 3, 1, 2, 3]));
