d04ea6b1722d1d447e3381e8a681e69052a14156
choongnyeong6215 <choong6215@gmail.com>
2024-02-17 20:36:48 +0900

    section2 - 6,7 2차원 배열 재풀이

--- section2/6.js
+++ section2/6.js
@@ -1,35 +1,34 @@
 function solution(arr) {
+  let maxSum = 0;
+  let rowSum = 0; // 행 합계
+  let colSum = 0; // 열 합계
+  let lCrossSum = 0; // ↘︎ 대각선 합
+  let rCrossSum = 0; // ↖︎ 대각선 합
 
-    let maxNum = Number.MIN_SAFE_INTEGER;
-    let rowSum = colSum = lCrossSum = rCrossSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // 행, 열 합계 초기화
+    rowSum = colSum = 0;
 
-    for(let i = 0; i < arr.length; i++) {
-        
-        // 초기화
-        rowSum = 0;
-        colSum = 0;
+    lCrossSum += arr[i][i];
+    rCrossSum += arr[i][arr[i].length - i - 1];
 
-        for(let j = 0; j < arr[i].length; j++) {
-            rowSum += arr[i][j];
-            colSum += arr[j][i];
-
-            if(i === j) {
-                lCrossSum += arr[i][j]; // left top - right bottom
-                rCrossSum += arr[i][arr.length - i -1]  // right top - left bottom
-            }
-
-            maxNum = Math.max(maxNum, rowSum, colSum, lCrossSum, rCrossSum);
-        }
+    for (let j = 0; j < arr[i].length; j++) {
+      rowSum += arr[i][j];
+      colSum += arr[j][i];
     }
-    return maxNum;
+    maxSum = Math.max(rowSum, colSum, lCrossSum, rCrossSum);
+  }
+
+  return maxSum;
 }
 
-
 // test case
-console.log(solution([
+console.log(
+  solution([
     [10, 13, 10, 12, 15],
     [12, 39, 30, 23, 11],
     [11, 25, 50, 53, 15],
     [19, 27, 29, 37, 27],
-    [19, 13, 30, 13, 19]
-]));
+    [19, 13, 30, 13, 19],
+  ])
+);
--- section2/7.js
+++ section2/7.js
@@ -1,34 +1,32 @@
 function solution(arr) {
-    let cnt = 0;
-    for(let i = 0; i < arr.length; i++) {
-        for(let j = 0; j < arr[i].length; j++) {
-            const stdArr = [];
-            const stdNum = arr[i][j];
-            const left = arr[i][j-1] ?? 0;
-            const right = arr[i][j+1] ?? 0;
-            const top = i > 0 ? arr[i-1][j] : 0;
-            const bottom = i < arr.length - 1 ? arr[i+1][j] : 0; 
+  let bongCnt = 0;
 
-            stdArr.push(stdNum);
-            stdArr.push(left);
-            stdArr.push(right);
-            stdArr.push(top);
-            stdArr.push(bottom);
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      let stdBong = arr[i][j]; // 현재 봉우리
+      let topNum = i > 0 ? arr[i][j] : 0; // 상
+      let bottomNum = i < arr.length - 1 ? arr[i + 1][j] : 0; // 하
+      let leftNum = arr[i][j - 1] ?? 0; // 좌
+      let rightNum = arr[j][j + 1] ?? 0; // 우
 
-            if(stdNum === stdArr.sort((a, b) => b - a)[0]) {
-                cnt++;
-            }
-        }
+      let BongArr = [stdBong, topNum, bottomNum, leftNum, rightNum];
+
+      if (stdBong === BongArr.sort((a, b) => b - a)[0]) {
+        bongCnt++;
+      }
     }
-    return cnt;
+  }
+
+  return bongCnt;
 }
 
-
 // test case
-console.log(solution([
+console.log(
+  solution([
     [5, 3, 7, 2, 3],
     [3, 7, 1, 6, 1],
     [7, 2, 5, 3, 4],
     [4, 3, 6, 4, 1],
-    [8, 7, 3, 5, 2]
-])); 
+    [8, 7, 3, 5, 2],
+  ])
+);
